1c02d06216b8b223e1db3ddc28ac95f152b8ede8
seo02wow <rlatjdud9188@naver.com>
2024-05-25 14:57:27 +0900

    [2-2] 원시 타입

---
+++ onebite-typescript/section2/src/chapter1.ts
@@ -0,0 +1,28 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello";
+let str2: string = `hello`;
+let str3: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let ull1: null = null;
+
+// undefined
+let nude1: undefined = undefined;
+
+// 리터럴 타입 - 값 자체가 타입이 되는 타입
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
