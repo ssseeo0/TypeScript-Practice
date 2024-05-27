41bbcf12f78890a3ce7122d1fd72715686aed051
seo02wow <rlatjdud9188@naver.com>
2024-05-27 17:58:48 +0900

    [2-3] 객체

---
+++ onebite-typescript/section2/src/chapter3.ts
@@ -0,0 +1,18 @@
+// object
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "김서영",
+};
+
+user = {
+  name: "김순구",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: "MY API KEY",
+};
